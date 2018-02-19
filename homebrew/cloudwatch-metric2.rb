0e670cb9241e5fbe00c1dca2b60a45275cbb9ff0
winebarrel <sugawara@winebarrel.jp>
2018-02-19 12:25:21 +0900

    Fix brew formula

--- homebrew/cloudwatch-metric2.rb
+++ homebrew/cloudwatch-metric2.rb
@@ -5,7 +5,7 @@ class CloudwatchMetric2 < Formula
 
   homepage 'https://github.com/winebarrel/cloudwatch-metric2'
   url "https://github.com/winebarrel/cloudwatch-metric2/releases/download/v#{VERSION}/cloudwatch-metric2-v#{VERSION}-darwin-amd64.gz"
-  sha256 '...'
+  sha256 '9b5ced0d44fcf7f9b13dcf5e1f0a12c425a287f495032c372fa51283410d5425'
   version VERSION
   head 'https://github.com/winebarrel/cloudwatch-metric2.git', :branch => 'master'
 
